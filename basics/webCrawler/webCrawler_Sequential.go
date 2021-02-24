a82307f39a3bfa29208b72c293d4753062a8689c
Yazan Diranieh <yazanmd@yahoo.com>
2021-02-24 21:36:37 +0000

    sequential web crawling

---
+++ basics/webCrawler/webCrawler_Sequential.go
@@ -0,0 +1,35 @@
+package webCrawler
+
+import (
+	sequential "ConcurrenziGo/collections"
+	"fmt"
+)
+
+// Web crawling using breadth-first traversal
+func CrawlSequentially(topUrl string) {
+	// This map is used to ensure that we do not crawl the same url twice
+	processed := make(map[string]bool)
+
+	// Used in breadth traversal
+	var queue = sequential.CreateQueue()
+	queue.Enqueue(topUrl)
+
+	for !queue.IsEmpty() {
+		item, err := queue.Dequeue()
+		if err != nil {
+			fmt.Printf("Failed to queue item: %v", err)
+			continue
+		}
+
+		// Crawl this url and return a list of new urls to crawl
+		url := item.(string)
+		links := crawlUrl(url)
+		processed[url] = true
+		for _, link := range links {
+			// Queue this link if not processed before
+			if !processed[link] {
+				queue.Enqueue(link)
+			}
+		}
+	}
+}
