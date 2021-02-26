ace360adceeb225ee47bafec838fed70e2f98fab
Yazan Diranieh <yazanmd@yahoo.com>
2021-02-26 21:40:11 +0000

    Main application runner

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"ConcurrenziGo/basics/nonBlockingCache"
+	"flag"
+	"fmt"
+)
+
+func main() {
+	// To run: go run main.go -port 2222 -maxConn 2
+	// Read port number from command line argument
+	var port *int = flag.Int("port", 8000, "-port 1234")
+	var maxConnections *int = flag.Int("maxConn", 2, "-maxConn 2")
+	flag.Parse()
+	fmt.Printf("Listening on port %d. Maximum number of connections =%d \n", *port, *maxConnections)
+
+	//clockServer.Run(*port, *maxConnections)
+	//echoServer.Run(*port, *maxConnections)
+	//pipelines.Run()
+	//pipelines.RunWithRange()
+	//pipelines.RunConcurrentStages()
+	//webCrawler.CrawlSequentially("https://golang.org")
+	//webCrawler.CrawlConcurrently("https://golang.org")
+	//counters.AbortableCountdown(10);
+	//diretoryTraversal.TraverseRoots([]string{"C:\\Temp"})
+	//diretoryTraversal.TraverseRootsAndMultiplexResults([]string{"C:\\Temp"})
+	//chatServer.Run(*port)
+	nonBlockingCache.RunConcurrent()
+
+	fmt.Println("Completed")
+}
